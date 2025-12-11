eee0a8400e6a04b702382fc0cfc80d151664af31
Dsara16 <dambrosiosara86@gmail.com>
2025-12-11 22:38:57 +0100

    Update segreto.js

--- segreto.js
+++ segreto.js
@@ -1,6 +1,10 @@
-const button = document.getElementById("bottoneSegreto");
+/*const button = document.getElementById("bottoneSegreto");
 const result = document.getElementById("resultTex");
 
 button.addEventListener("click", () => {
   result.style.display = 'block';
-});
+});*/
+
+function mostraFase(){
+  document.getElementById("messaggio").innerHTML= "Oh no, cliccando tale bottone ti sei offerto volontatio agli Hunger Games D: ... Ti consigliamo di scappare, se riesci, altrimenti prendi arco e frecce e preparati alla sopravvivenza!";
+}
