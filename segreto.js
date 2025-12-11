33e683a02eb1bc2057368cdc4f203d45c7f74837
Dsara16 <dambrosiosara86@gmail.com>
2025-12-11 22:23:45 +0100

    Update segreto.js

--- segreto.js
+++ segreto.js
@@ -1,6 +1,6 @@
 const button = document.getElementById("bottoneSegreto");
 const result = document.getElementById("resultTex");
 
-bottoneSegreto.addEventListener("click", () => {
+button.addEventListener("click", () => {
   result.style.display = 'block';
 });
